cc64f06fbfee497ba01e2a0499769f2c3586d6f3
ehroy <anjaykoko69@gmail.com>
2025-11-21 19:04:32 +0700

    seo friendly

--- database/migrations/2025_11_21_084921_add_slug_to_products_table.php
+++ database/migrations/2025_11_21_084921_add_slug_to_products_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('slug')->unique()->after('name');
+            $table->string('slug')->unique()->after('nama');
         });
     }
 
